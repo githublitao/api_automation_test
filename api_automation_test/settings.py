f562525f07ab3befe6341434e4d28466d07231c5
githublitao <18202886913@163.com>
2018-03-29 16:28:48 +0800

    1。修改数据库配置

--- api_automation_test/settings.py
+++ api_automation_test/settings.py
@@ -151,7 +151,7 @@ DATABASES = {
         'NAME': 'api_test',
         'USER': 'root',
         'PASSWORD': '123456',
-        'HOST': '192.168.37.131',
+        'HOST': '192.168.89.117',
         'PORT': '3306',
     }
 }
