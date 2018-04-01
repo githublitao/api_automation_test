f7eb584c258054518c2d13a64b1a5f2757f85b59
litao <18202886913@163.com>
2018-04-01 17:57:33 +0800

    1.python 命令行中运行py文件，找不到包的bug 2.外部通django连接数据库，导入django配置

--- api_test/common/auto_test.py
+++ api_test/common/auto_test.py
@@ -1,4 +1,14 @@
+import django
 import sys
+import os
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+PathProject = os.path.split(rootPath)[0]
+sys.path.append(rootPath)
+sys.path.append(PathProject)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_automation_test.settings")
+django.setup()
 
 from api_test.models import AutomationCaseApi
 
