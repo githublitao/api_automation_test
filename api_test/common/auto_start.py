ebfe3ad34b0db076c23b8a212217684cbbdb294e
githublitao <18202886913@163.com>
2018-05-14 18:24:46 +0800

    1.修改django—setting 允许hosts为'*'

--- api_test/common/auto_start.py
+++ api_test/common/auto_start.py
@@ -4,9 +4,9 @@ import sys
 
 def task_start_timing():
     my_user_cron = CronTab(user=True)
-    job = my_user_cron.new(command='/usr/local/python3/bin/python3 /var/lib/jenkins/workspace//var/lib/jenkins/workspace/api_automation_test_master-JU72M6SAEYKDY6SN3LUUPLXPTX3F35MVFZ57J4JE3I5TJCTRFXHQ/'
-                                   'api_test/common/auto_test.py %s %s %s >> /var/lib/jenkins/task/%s.log'
-                                   % (sys.argv[3], sys.argv[8], sys.argv[9], sys.argv[8]))
+    job = my_user_cron.new(command='/usr/local/python3/bin/python3 /var/lib/jenkins/workspace/api_automation_test_master-JU72M6SAEYKDY6SN3LUUPLXPTX3F35MVFZ57J4JE3I5TJCTRFXHQ/'
+                                   'api_test/common/auto_test.py %s %s  >> /var/lib/jenkins/task/%s.log'
+                                   % (sys.argv[3], sys.argv[8], sys.argv[8]))
     job.set_comment(sys.argv[5])
     if sys.argv[2] == 'm':
         _time = '*/%s * * * *' % sys.argv[1]
@@ -20,7 +20,7 @@ def task_start_timing():
     my_user_cron.write()
 
     end_task = CronTab(user=True)
-    jobs = end_task.new(command='/usr/local/python3/bin/python3 /var/lib/jenkins/workspace//var/lib/jenkins/workspace/api_automation_test_master-JU72M6SAEYKDY6SN3LUUPLXPTX3F35MVFZ57J4JE3I5TJCTRFXHQ/'
+    jobs = end_task.new(command='/usr/local/python3/bin/python3 /var/lib/jenkins/workspace/api_automation_test_master-JU72M6SAEYKDY6SN3LUUPLXPTX3F35MVFZ57J4JE3I5TJCTRFXHQ/'
                                 'api_test/common/end_task.py %s >> /var/lib/jenkins/task/%s.log'
                                 % (sys.argv[8], sys.argv[8]))
     jobs.set_comment(sys.argv[5]+"_结束")
