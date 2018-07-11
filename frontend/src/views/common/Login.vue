1ae00b03a0727e7d7687476bb1c1423edc4a1ac6
litao <18202886913@163.com>
2018-07-11 18:30:27 +0800

    从新编写登录页面样式

--- frontend/src/views/common/Login.vue
+++ frontend/src/views/common/Login.vue
@@ -1,18 +1,41 @@
 <template>
-  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
-    <h3 class="title">系统登录</h3>
-    <el-form-item prop="account">
-      <el-input type="text" v-model.trim="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
-    </el-form-item>
-    <el-form-item prop="checkPass">
-      <el-input type="password" v-model.trim="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
-    </el-form-item>
-    <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
-    <el-form-item style="width:100%;">
-      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
-      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
-    </el-form-item>
-  </el-form>
+    <div class="homeBox">
+        <ul class="tyg-div">
+            <li>让</li>
+            <li><div style="margin-left:20px;">测</div></li>
+            <li><div style="margin-left:40px;">试</div></li>
+            <li><div style="margin-left:60px;">变</div></li>
+            <li><div style="margin-left:80px;">得</div></li>
+            <li><div style="margin-left:100px;">轻</div></li>
+            <li><div style="margin-left:120px;">松</div></li>
+        </ul>
+        <div style="width:32%;height: auto;margin-left: 30%">
+            <div class="title0">自动化测试平台</div>
+            <div class="title1">项目管理、接口管理、用例管理、测试报告、任务设置</div>
+            <div class="lun-container">
+                <div class="carouse" id="carouse">
+                    <div class="pic1"><img src="../../assets/page1_0.png" alt="pic1"></div>
+                    <div class="pic2"><img src="../../assets/page1_1.png" alt="pic2"></div>
+                    <div class="pic3"><img src="../../assets/page1_2.png" alt="pic3"></div>
+                </div>
+            </div>
+            <img class="img-login" src="../../assets/page1_3.jpg"/>
+        </div>
+        <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+            <h3 class="title">系统登录</h3>
+            <el-form-item prop="account">
+                <el-input type="text" v-model.trim="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+            </el-form-item>
+            <el-form-item prop="checkPass">
+                <el-input type="password" v-model.trim="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+            </el-form-item>
+            <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
+            <el-form-item style="width:100%;">
+                <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
+                <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 
 <script>
@@ -179,35 +202,161 @@
                     },
                 })
                 // document.getElementById('tip').innerHTML = '定位失败';
+            },
+            carouselPicture(){
+                this.ab(1)
+                // this.ab(2)
+                // this.ab(3)
+            },
+            ab(num){
+                var carouse = document.getElementsByClassName("carouse")
+                carouse.item(0).id = 'carouse'+num;
             }
+
         },
         mounted() {
             this.getVisitor();
+            this.carouselPicture()
         }
     }
 
 </script>
 
 <style lang="scss" scoped>
+    .homeBox {
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        top: 0px;
+        background-color: #191c2c;
+    }
   .login-container {
     /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
+      position: absolute;
     -webkit-border-radius: 5px;
     border-radius: 5px;
     -moz-border-radius: 5px;
     background-clip: padding-box;
-    margin: 180px auto;
-    width: 350px;
+    /*margin: 180px auto;*/
+      /*margin-top: 10%;*/
+      /*right: 50px;*/
+    width: 300px;
     padding: 35px 35px 15px 35px;
-    background: #fff;
+    background: #23305a;
     border: 1px solid #eaeaea;
     box-shadow: 0 0 25px #cac6c6;
+      z-index: 1000;
+    float: right;
+    right: 4%;
+    top: 25%;
     .title {
       margin: 0px auto 40px auto;
       text-align: center;
-      color: #505458;
+      color: #2ec0f6;
     }
     .remember {
       margin: 0px 0px 35px 0px;
+        color: #eaeaea;
     }
   }
+    .img-login {
+        margin-top: -35%;
+        width: 100%;
+        height: auto;
+    }
+    .title0 {
+        position: absolute;
+        top: 10%;
+        left: -41px;
+        width: 100%;
+        text-align: center;
+        color: #2ec0f6;
+        font-size: 40px;
+        height: 70px;
+        line-height: 70px;
+        /*<!--margin: -300px 0 0 0;-->*/
+        z-index: 1000;
+    }
+    .title1 {
+        position: absolute;
+        top: 16%;
+        left: -41px;
+        width: 100%;
+        text-align: center;
+        color: #eaeaea;
+        font-size: 20px;
+        height: 70px;
+        line-height: 70px;
+        /*<!--margin: -300px 0 0 0;-->*/
+        z-index: 1000;
+    }
+    .tyg-div {
+        color: #2ec0f6;
+        z-index: -1000;
+        float: left;
+        position: absolute;
+        left: 5%;
+        top: 20%;
+        font-size: 30px;
+        list-style-type:none
+    }
+    .lun-container{
+        width: 210px;
+        height:140px;
+        position: relative;
+        font-size: 32px;
+        color: #FFFFFF;
+        text-align: center;
+        line-height: 90px;
+        margin: 200px auto;
+        margin-bottom: 0px;
+        margin-top:48%;
+        perspective: 1000px;
+        z-index: 1000;
+    }
+    .carouse{
+        transform-style:preserve-3d;
+
+    }
+    .carouse div{
+        display: block;
+        position: absolute;
+        width: 140px;
+        height: 90px;
+    }
+
+    .carouse .pic1{
+        transform: rotateY(0deg) translateZ(160px);
+    }
+    .carouse .pic2{
+        transform: rotateY(120deg) translateZ(160px);
+    }
+    .carouse .pic3{
+        transform: rotateY(240deg) translateZ(160px);
+    }
+
+    /*=== 下一个动画 ===*/
+    @keyframes to-scroll1 {
+        0%{
+            transform: rotateY(0deg);
+        }
+
+        33%{
+            transform: rotateY(-120deg);
+
+        }
+        66%{
+            transform: rotateY(-240deg);
+
+        }
+        100%{
+            transform: rotateY(-360deg);
+
+        }
+
+    }
+    #carouse1{
+        animation: to-scroll1  10s ease infinite;
+        /*animation-fill-mode: both;*/
+    }
 </style>
