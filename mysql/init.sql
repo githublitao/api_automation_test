82eed23bba185e648eae8b8f1f3a4247d21feb22
taol <tao.li1@jiediankeji.com>
2020-09-08 13:26:32 +0800

    docker

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,6 +1,6 @@
 CREATE USER 'root'@'%' IDENTIFIED BY '123456';
-GRANT All privileges ON *.* TO 'root'@'%';
-update mysql.user set authentication_string=password('123456') where user='root';
 CREATE DATABASE IF NOT EXISTS api_test
 DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_unicode_ci;
+GRANT All privileges ON *.* TO 'root'@'%';
+update mysql.user set authentication_string=password('123456') where user='root';
