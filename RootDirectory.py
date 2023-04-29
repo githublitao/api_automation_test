8ca7079d62ece42e320aa956b24c778ae83865b5
Hedley <18202886913@163.com>
2023-04-29 20:04:01 +0800

    add RootDirectory.py

---
+++ RootDirectory.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+# @Author  : litao
+
+# @Project : api_automation_test
+
+# @FileName: RootDirectory.py
+
+# @Software: PyCharm
+import os
+import sys
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+JOB_TIME = 50
