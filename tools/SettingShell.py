5bdafed7cfa985520734b732d5459e84b2afc734
Hedley <18202886913@163.com>
2023-05-25 13:14:01 +0800

    add SettingShell.py

---
+++ tools/SettingShell.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+# @Author  : litao
+
+# @Project : api_automation_test
+
+# @FileName: SettingShell.py
+
+# @Software: PyCharm
+from tools.util.CreateRequirements import create_requirements
+
+# create_requirements()
