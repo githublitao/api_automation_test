c9ed9488ff54b6eb06022aae8ac0c1fac539325a
Hedley <18202886913@163.com>
2024-04-05 08:02:01 +0800

    add SettingShell.py

---
+++ api_test/SettingShell.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# @Author  : litao
+
+# @Project : api_automation_test
+
+# @FileName: SettingShell.py
+
+# @Software: PyCharm
+from api_test.utils.CompenSation import compensation
+
+compensation()
