136edfc7890ce996b8a0e03128bc68b042c1feb1
Hedley <18202886913@163.com>
2024-08-24 20:04:01 +0800

    add DingdingConfig.py

---
+++ api_test/config/DingdingConfig.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# @Author  : litao
+
+# @Project : api_automation_test
+
+# @FileName: DingdingConfig.py
+
+# @Software: PyCharm
+# 钉钉开发平台 open-dev.dingtalk.com
+APPID = ''
+
+APPSECRET = ''
